0457ceed35fc358f9f2f0b00eec439e120371a9b
Yashwanth Gundlapally <g.yashwanth896@gmail.com>
2023-07-15 22:25:02 -0400

    code

---
+++ src/main/java/com/imgyash/cicd/GithubCicdActionsApplication.java
@@ -0,0 +1,21 @@
+package com.imgyash.cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GithubCicdActionsApplication {
+
+	@GetMapping("/welcome")
+	public String welcome(){
+		return "Welcome to Github Actions - CI/CD Pipeline";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubCicdActionsApplication.class, args);
+	}
+
+}
